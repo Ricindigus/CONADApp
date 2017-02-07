f55a17251d3cf1a3be5c52c487b8bf5c7cb833e3
Ricardo <ricindigus@gmail.com>
2017-02-06 19:32:26 -0500

    slider aut

--- app/src/main/java/com/example/ricardo/conadapp/PasosASeguirActivity.java
+++ app/src/main/java/com/example/ricardo/conadapp/PasosASeguirActivity.java
@@ -1,9 +1,12 @@
 package com.example.ricardo.conadapp;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -12,13 +15,16 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -26,158 +32,190 @@ import com.squareup.picasso.Picasso;
 
 public class PasosASeguirActivity extends AppCompatActivity {
 
-    private SectionsPagerAdapter mSectionsPagerAdapter;
-    private ViewPager mViewPager;
-    private int fragmentActual = 1;
-    private float x1,x2;
-    private FloatingActionButton fabAtras;
-    private FloatingActionButton fabAdelante;
-    static final int MIN_DISTANCE = 150;
+    private ViewPager viewPager;
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private String[] numeros = {"&#10102;", "&#10103;", "&#10104;", "&#10105;"};
+    private int[] layouts;
+    private Button btnSkip, btnNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pasos_aseguir);
-
-        showToolbar("AUT",true);
         if (android.os.Build.VERSION.SDK_INT >= 21) {
             Window window = this.getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(ContextCompat.getColor(getApplicationContext(), R.color.aut_primary_dark));
         }
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarApp);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("¿Qué pasos debo seguir?");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        btnSkip = (Button) findViewById(R.id.btn_skip);
+        btnNext = (Button) findViewById(R.id.btn_next);
 
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        // layouts of all welcome sliders
+        // add few more layouts if you want
+        layouts = new int[]{
+                R.layout.welcome_slide1,
+                R.layout.welcome_slide2,
+                R.layout.welcome_slide3,
+                R.layout.welcome_slide4};
 
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.aut_container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
+        // adding bottom dots
+        addBottomDots(0);
 
-        fabAtras = (FloatingActionButton) findViewById(R.id.fab_atras);
-        fabAdelante = (FloatingActionButton) findViewById(R.id.fab_adelante);
+        // making notification bar transparent
+//        changeStatusBarColor();
 
-        fabAtras.setOnClickListener(new View.OnClickListener() {
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+        btnSkip.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                fragmentActual = mViewPager.getCurrentItem();
-                if(fragmentActual > 0) {
-                    mViewPager.setCurrentItem(fragmentActual - 1);
-                    fragmentActual = mViewPager.getCurrentItem() + 1;
-                }
+            public void onClick(View v) {
+//                launchHomeScreen();
+                int current = getItem(-1);
+                if (current > -1) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);}
+//                } else {
+//                    launchHomeScreen();
+//                }
             }
         });
-        fabAdelante.setOnClickListener(new View.OnClickListener() {
+
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                fragmentActual = mViewPager.getCurrentItem();
-                if(fragmentActual < 3) {
-                    mViewPager.setCurrentItem(fragmentActual + 1);
-                    fragmentActual = mViewPager.getCurrentItem() + 1;
-                }
+            public void onClick(View v) {
+                // checking for last page
+                // if last page home screen will be launched
+                int current = getItem(+1);
+                if (current < layouts.length) {
+                    // move to next screen
+                    viewPager.setCurrentItem(current);}
+//                } else {
+//                    launchHomeScreen();
+//                }
             }
         });
     }
 
-    public static class PlaceholderFragment extends Fragment {
-        private static final String ARG_SECTION_NUMBER = "section_number";
-        private int[] textos = {R.string.aut_paso1, R.string.aut_paso2, R.string.aut_paso3
-                , R.string.aut_paso4};
-        private int[] imagenes = {R.drawable.formulario_aut, R.drawable.correo_electronico, R.drawable.panel_expertos
-                , R.drawable.conad_informara};
-        public PlaceholderFragment() {
-        }
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-        @SuppressLint("NewApi")
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[layouts.length];
 
-            View rootView = rootView = inflater.inflate(R.layout.fragment_pasos_a_seguir, container, false);
-            int pagina = getArguments().getInt(ARG_SECTION_NUMBER);
-            TextView textView1;
-            TextView textView2;
-            RelativeLayout relativeLayout;
-            switch (pagina) {
-                case 1:
-                    textView1 = (TextView) rootView.findViewById(R.id.txt_descripcion_paso);
-                    textView1.setText(getString(textos[pagina-1]));
-                    textView2 = (TextView) rootView.findViewById(R.id.txt_numero_paso);
-                    textView2.setText("PASO 1");
-                    relativeLayout = (RelativeLayout) rootView.findViewById(R.id.layout_pasos);
-                    relativeLayout.setBackground(ContextCompat.getDrawable(getContext(), imagenes[pagina-1]));
+        int[] colorsActive = getResources().getIntArray(R.array.array_dot_active);
+        int[] colorsInactive = getResources().getIntArray(R.array.array_dot_inactive);
 
-                    break;
-                case 2:
-                    textView1 = (TextView) rootView.findViewById(R.id.txt_descripcion_paso);
-                    textView1.setText(getString(textos[pagina-1]));
-                    textView2 = (TextView) rootView.findViewById(R.id.txt_numero_paso);
-                    textView2.setText("PASO 2");
-                    relativeLayout = (RelativeLayout) rootView.findViewById(R.id.layout_pasos);
-                    relativeLayout.setBackground(ContextCompat.getDrawable(getContext(), imagenes[pagina-1]));
-                    break;
-                case 3:
-                    textView1 = (TextView) rootView.findViewById(R.id.txt_descripcion_paso);
-                    textView1.setText(getString(textos[pagina-1]));
-                    textView2 = (TextView) rootView.findViewById(R.id.txt_numero_paso);
-                    textView2.setText("PASO 3");
-                    relativeLayout = (RelativeLayout) rootView.findViewById(R.id.layout_pasos);
-                    relativeLayout.setBackground(ContextCompat.getDrawable(getContext(), imagenes[pagina-1]));
-                    break;
-                case 4:
-                    textView1 = (TextView) rootView.findViewById(R.id.txt_descripcion_paso);
-                    textView1.setText(getString(textos[pagina-1]));
-                    textView2 = (TextView) rootView.findViewById(R.id.txt_numero_paso);
-                    textView2.setText("PASO 4");
-                    relativeLayout = (RelativeLayout) rootView.findViewById(R.id.layout_pasos);
-                    relativeLayout.setBackground(ContextCompat.getDrawable(getContext(), imagenes[pagina-1]));
-                    break;
-                default: break;
-            }
-            return rootView;
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml(numeros[i]) + " ");
+            dots[i].setTextSize(23);
+            dots[i].setTextColor(colorsInactive[currentPage]);
+            dotsLayout.addView(dots[i]);
         }
+        dots[currentPage].setTextColor(colorsActive[currentPage]);
+//        if (dots.length > 0)
+//            dots[currentPage].setTextColor(colorsActive[currentPage]);
     }
 
-    public class SectionsPagerAdapter extends FragmentPagerAdapter {
-        public SectionsPagerAdapter(FragmentManager fm) {
-            super(fm);
-        }
+    private int getItem(int i) {
+        return viewPager.getCurrentItem() + i;
+    }
+
+    //  viewpager change listener
+    ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
         @Override
-        public Fragment getItem(int position) {
-            return PlaceholderFragment.newInstance(position + 1);
+        public void onPageSelected(int position) {
+            addBottomDots(position);
+            btnNext.setText(getString(R.string.next));
+            btnSkip.setVisibility(View.VISIBLE);
+            // changing the next button text 'NEXT' / 'GOT IT'
+//            if (position == layouts.length - 1) {
+            // last page. make button text to GOT IT
+//                btnNext.setText(getString(R.string.start));
+//                btnSkip.setVisibility(View.GONE);
+//            } else {
+            // still pages are left
+            btnNext.setText(getString(R.string.next));
+            btnSkip.setVisibility(View.VISIBLE);
+//            }
         }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int arg0) {
+
+        }
+    };
+
+    /**
+     * Making notification bar transparent
+     */
+//    private void changeStatusBarColor() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            Window window = getWindow();
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
+//        }
+//    }
+
+    /**
+     * View pager adapter
+     */
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(layouts[position], container, false);
+            if(position == 0){
+                FloatingActionButton fab = (FloatingActionButton) view.findViewById(R.id.fab_descarga_aut);
+                fab.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                .setAction("Action", null).show();
+                    }
+                });
+            }
+            container.addView(view);
+            return view;
+        }
+
         @Override
         public int getCount() {
-            return 4;
+            return layouts.length;
         }
+
         @Override
-        public CharSequence getPageTitle(int position) {
-            switch (position) {
-                case 0:
-                    return "SECTION 1";
-                case 1:
-                    return "SECTION 2";
-                case 2:
-                    return "SECTION 3";
-                case 3:
-                    return "SECTION 4";
-            }
-            return null;
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
         }
-    }
-    public void showToolbar(String title, boolean upButton){
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarApp);
-        toolbar.setBackgroundColor(ContextCompat.getColor(getApplicationContext(), R.color.aut_primary));
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle(title);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
     }
 
 }
